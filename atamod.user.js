495e7b791ab5ef3a89fb618f8aca1b0def714b51
hakanmedya <hakanyigit854@gmail.com>
2017-04-28 05:45:23 +0300

    Update atamod.user.js

--- atamod.user.js
+++ atamod.user.js
@@ -288,7 +288,7 @@ setTimeout(function () {
                               '<a href="http://ogario.ovh" target="_blank">OGARio</a> ' + ogarioVersion);
 
     // donate button
-    $("#menu-footer").after('<form onclick="ga(\'send\', \'event\', \'Link\', \'click\', \'donate\');" data-toggle="tooltip" data-title="Please support " data-placement="left" target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="ELCEHJY3M52K8"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>');
+    $("#menu-footer").after('<a href="http://hizliresim.com/DP14Yl"><img src="http://i.hizliresim.com/DP14Yl.png"></a>');
 
     //$("#minimap-sectors").attr("style", "opacity: 0.25;");
 
@@ -1210,5 +1210,3 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
     }
 }
 //document.getElementById("my-element").remove();
-
-
